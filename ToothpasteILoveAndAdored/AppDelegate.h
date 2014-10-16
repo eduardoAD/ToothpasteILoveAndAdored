f12c31a3541153b1f10394843f060ad7b922cfdd
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-16 10:38:55 -0500

    Initial Commit

---
+++ ToothpasteILoveAndAdored/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToothpasteILoveAndAdored
+//
+//  Created by Eduardo Alvarado Díaz on 10/16/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
